eded1c653fa847d3e24079ce56bb872ab39d2cb9
AmrHE <amr.hassan.emam@gmail.com>
2021-11-25 13:11:35 +0200

    stateless component

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,39 +1,46 @@
-import React, { Component } from "react";
+import React from "react";
 import Book from "./Book";
+import PropTypes from "prop-types";
 
-class Shelf extends Component {
-	render() {
-		const books = this.props.books;
-		// console.log(books.map((book) => book.id));
+const Shelf = (props) => {
+	if (props.length > 0) {
+		console.log(props);
+	}
+	const { books } = props;
+	console.log(books);
 
-		return (
-			<div className="list-books-content">
-				<div>
-					<div className="bookshelf">
-						<h2 className="bookshelf-title">{this.props.title}</h2>
-						<div className="bookshelf-books">
-							<ol className="books-grid">
-								{books.length > 0 &&
-									books.map((book) => (
-										<Book
-											book={book}
-											key={book.id}
-											// id={book.id}
-											// name={book.title}
-											// img={book.imageLinks.thumbnail}
-											// imgSmall={book.imageLinks.smallThumbnail}
-											// author={book.authors}
-											// shelf={book.shelf}
-											moveBook={this.props.moveBook}
-										/>
-									))}
-							</ol>
-						</div>
+	return (
+		<div className="list-books-content">
+			<div>
+				<div className="bookshelf">
+					<h2 className="bookshelf-title">{props.title}</h2>
+					<div className="bookshelf-books">
+						<ol className="books-grid">
+							{books.length > 0 &&
+								books.map((book) => (
+									<Book
+										book={book}
+										key={book.id}
+										// id={book.id}
+										// name={book.title}
+										// img={book.imageLinks.thumbnail}
+										// imgSmall={book.imageLinks.smallThumbnail}
+										// author={book.authors}
+										// shelf={book.shelf}
+										moveBook={props.moveBook}
+									/>
+								))}
+						</ol>
 					</div>
 				</div>
 			</div>
-		);
-	}
-}
+		</div>
+	);
+};
+
+Shelf.propTypes = {
+	books: PropTypes.arrayOf(PropTypes.object),
+	movedBook: PropTypes.func,
+};
 
 export default Shelf;
